70b4705c5045145ea1e1bafc4afbdda11b657e76
Timothy Goon <t.kgoonx@gmail.com>
2021-09-27 17:54:53 -0400

    Added cors for development and put in a basic fetch api call to make sure it works.

--- server/app.js
+++ server/app.js
@@ -4,12 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-require('dotenv').config();
+var env = process.env.NODE_ENV || 'development'
+
+var app = express();
+
+if (env === 'development') {
+  var cors = require('cors');
+  require('dotenv').config();
+  app.use(cors());
+  console.log('cors enabled');
+}
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,12 +29,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
