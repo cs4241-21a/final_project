4b1f7b22723bd8658d99f87233171511e4d41f21
Glitch (glitch-hello-website) <none>
2021-10-03 15:53:50 +0000

    🐉🔑 Checkpoint ./public/js/loginscript.js:35541864/1656 ./server.js:35541864/698

---
+++ public/js/loginscript.js
@@ -0,0 +1,59 @@
+const login = function(e) {
+  e.preventDefault();
+
+  const username = document.querySelector("#uname"),
+    password = document.querySelector("#pword");
+
+  if (username.value === "" || password.value === "") {
+    alert("Enter a valid username and password");
+    return false;
+  }
+
+  let json = { username: username.value, password: password.value },
+    body = JSON.stringify(json);
+
+  fetch("/login", {
+    method: "POST",
+    body: body,
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(response => response.json())
+    .then(user => {
+      if (user.length == 0) {
+        alert("Username or password is incorrect");
+      } else {
+        window.location.href = "game.html";
+      }
+    });
+  return false;
+};
+
+const register = function(e) {
+  e.preventDefault();
+
+  const username = document.getElementById("uname"),
+    password = document.getElementById("pword");
+
+  if (username.value === "" || password.value === "") {
+    alert("Enter a valid username and password");
+    return false;
+  }
+
+  let json = { username: username.value, password: password.value },
+    body = JSON.stringify(json);
+
+  fetch("/register", {
+    method: "POST",
+    body: body,
+    headers: { "Content-Type": "application/json" }
+  }).then(response => response.json());
+  window.location.href = "game.html";
+};
+
+window.onload = function() {
+  const loginBtn = document.getElementById("loginButton");
+  loginBtn.onclick = login;
+
+  const signUpBtn = document.getElementById("registerButton");
+  signUpBtn.onclick = register;
+};
--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const express = require('express'),
+      mongodb = require("mongodb"),
+      bodyParser = require("body-parser"),
       app = express(),
       port = 3001
 
 app.use('/', express.static(__dirname + '/'))
+app.use(bodyParser());
+
+const mongoclient = mongodb.MongoClient;
+const uri = "mongodb+srv://mike:" + process.env.MYPASSWORD + "@cluster0.guiga.mongodb.net/data?retryWrites=true&w=majority";
+const client = new mongodb.MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+let collection = null,
+    player = null,
+    playerInfo = null;
+
+client
+  .connect()
+  .then(() => {
+    return client.db("final").collection("game");
+  })
+  .then(_collection => {
+    collection = _collection;
+    return collection.find({}).toArray();
+  });
+
+
 
 app.listen(process.env.PORT || port);
