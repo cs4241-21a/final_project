60667ea803aa73ec1706f48a21ecfbf9bded40d1
chrisvieira <christophervieira1199@gmail.com>
2021-10-03 18:27:45 -0400

    Added skeleton files to main branch of project
    Moved starter files in a separate folder so they aren't confused with files developers are working on

--- server.js
+++ server.js
@@ -5,15 +5,37 @@ const express = require("express"),
   mongodb = require("mongodb"),
   MongoClient = mongodb.MongoClient,
   // Will need to create .env file with variables for DB_USER, DB_PASS, and DB_HOST
-  uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/HotelReviews?retryWrites=true&w=majority`,
+  // uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/HotelReviews?retryWrites=true&w=majority`,
+  uri = `mongodb+srv://test_user:tester_user_pw@cluster0.dpk53.mongodb.net/`,
   client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
-let collection = null;
-client.connect((err) => {
-  collection = client.db("enter db name").collection("enter collection name");
+  let collection_profile = null;
+  let collection_admin = null;
+  let collection_skill = null;
+  let collection_class = null;
+  let collection_post = null;
+  let collection_postComment = null;
+
+  let collection_studentSkillRelation = null;
+  let collection_studentClassRelation = null;
+  let collection_postSkillRelation = null;
+
+client.connect()
+
+client.once("open", () => {
+  collection_profile = client.db("WebwareGroupProject").collection("Profile");
+  collection_admin = client.db("WebwareGroupProject").collection("Admin");
+  collection_skill = client.db("WebwareGroupProject").collection("Skill");
+  collection_class = client.db("WebwareGroupProject").collection("Class");
+  collection_post = client.db("WebwareGroupProject").collection("Post");
+
+  collection_studentSkillRelation = client.db("WebwareGroupProject").collection("StudentSkillRelation");
+  collection_studentClassRelation = client.db("WebwareGroupProject").collection("StudentClassRelation");
+  collection_postSkillRelation = client.db("WebwareGroupProject").collection("PostSkillRelation");
+
 });
 
 // Middleware to check connection to database
