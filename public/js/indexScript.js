0544a4beb00155a759438ff976e1202c21781355
mrgagnon <mgagnon987@gmail.com>
2021-10-04 18:27:54 -0400

    changed basic drawing functionality to use p5

--- public/js/indexScript.js
+++ public/js/indexScript.js
@@ -1,54 +1,11 @@
 
-//bool to keep track if the user is currently drawing 
-let isDrawing = false
 let color = 'black'
-lineWidth = 3 
+let lineWidth = 50
 let eraseMode = false
-
-//Saves the mouse position 
-const getPos = function( event ) {
-    const canvas = document.querySelector('#canvas');
-    const rect = canvas.getBoundingClientRect()
-    cursorPosX = event.clientX - rect.left 
-    cursorPosY = event.clientY - rect.top 
-}
-
-//Signals to start drawing 
-const startDraw = function(event) {
-    isDrawing = true
-    getPos(event) 
-}
-
-//Signals to stop drawing 
-const stopDraw = function(event){
-    isDrawing = false
-}
-
-//Draws a line as the mouse moves when the mouse is pressed 
-const draw = function(event){
-    if (isDrawing === false){
-        return 
-    }
-    else {
-    ctx.strokeStyle = color //'black';
-
-    const checkErase = document.querySelector('input[name="action"]:checked').value
-    if(checkErase === "Erase"){  
-        ctx.strokeStyle = 'white'
-    }
-
-    console.log(lineWidth)
-    ctx.lineWidth = lineWidth
-    ctx.beginPath()
-    ctx.moveTo(cursorPosX, cursorPosY) 
-    getPos(event)
-    ctx.lineTo(cursorPosX,cursorPosY) // Possibly use a different shape like a circle or a square 
-    ctx.stroke()
-    }
-}
 
 const colorSelected = function(event){
     color = document.querySelector( '#colorSelector').value
+    console.log(color)
 }
 
 const changeWidth = function(event){
@@ -56,34 +13,38 @@ const changeWidth = function(event){
     lineWidth = document.querySelector( '#lineWidth').value
 }
 
-const clear = function(event){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-//Assigns methods to mouse events 
-window.onload = function() {
-    document.addEventListener('mousedown', startDraw)
-    document.addEventListener('mousemove',draw) 
-    document.addEventListener('mouseup', stopDraw)
+function setup() {
+    createCanvas(1000, 500);
 
     const colorSelector = document.querySelector( '#colorSelector')
     colorSelector.addEventListener('change', colorSelected);
 
     const clearBtn = document.querySelector( '#clearBtn')
-    clearBtn.onclick = clear
+    clearBtn.onclick = clearCanvas
 
     const lineWidth = document.querySelector('#lineWidth')
     lineWidth.onclick = changeWidth
-}
+  }
+  
+  function draw() {
+    if (mouseIsPressed) {
+        //Check is in erase mode 
+        const checkErase = document.querySelector('input[name="action"]:checked').value
+        if(checkErase === "Erase"){   //Todo change cursor to change area 
+            erase()
+            ellipse(mouseX, mouseY, lineWidth, lineWidth);
+       }
+        else{
+            stroke(color)
+            fill(color)
+            ellipse(mouseX, mouseY, lineWidth, lineWidth);
+       }
+    } 
+    noErase()
+  }
 
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
-
-let cursorPosX = 0
-let cursorPosY = 0 
-
-
-
-
+  function clearCanvas(){
+      clear()
+  }
 
 
