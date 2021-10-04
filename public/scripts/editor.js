25e4a390157779412d1658850be7aafcf3d49332
Matthew <entei678@gmail.com>
2021-10-04 16:50:41 -0400

    Figured out how to add text. Added favicon.

--- public/scripts/editor.js
+++ public/scripts/editor.js
@@ -2,6 +2,7 @@ let imgBtn = document.getElementById('imageInput')
 const download = document.getElementById('download')
 const canvas = document.getElementById('imgCanvas')
 let imgData
+let context
 
 download.addEventListener('click', function(e) {
     const link = document.createElement('a')
@@ -19,11 +20,18 @@ imgBtn.addEventListener('change', function(event) {
             const userImage = new Image()
             userImage.src = event.target.result
             userImage.onload = function (event) {
-                const context = canvas.getContext('2d')
+                context = canvas.getContext('2d')
                 canvas.width = userImage.width
                 canvas.height = userImage.height
                 context.drawImage(userImage,0,0)
-                imgData = canvas.toDataURL("image/jpeg",0.75)
+                context.font = "40pt Impact";
+                context.fillStyle = "white"
+                context.strokeStyle = "black"
+                context.textAlign = "center"
+
+                context.fillText("twitch.tv/plainttv",userImage.width/2,userImage.height/8);
+                context.strokeText("twitch.tv/plainttv",userImage.width/2,userImage.height/8);
+                imgData = canvas.toDataURL("image/png",0.75)
             }
         }
     }
