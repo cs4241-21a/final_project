4ed1fda8ebd43f56b1c8f04bd52914ca98e16243
imartinovic-dzaja <martinovic.ivan1m@gmail.com>
2021-10-04 13:49:18 -0400

    someWork

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import "./css/login_style.css"
+
 const Login = (props) => {
-  return <div>Login</div>;
+  return ( 
+    <div className = 'loginPage'>
+    <img id = 'logo' src = 'resources/logo.webp' alt = 'App Logo' width="300px" height="300px" title="App Logo"/>
+
+    <form id = 'form' action='/login' method='POST'>
+      <label for='username'> Username: </label><br/>  
+      <input type='text' id = 'username' name = 'username' required='required' pattern='[A-Za-z0-9]{1,20}'/> <br/>  
+
+      <label for='password'>Password:</label><br/>  
+      <input type='password' id='password' name='password' required='required' pattern='[A-Za-z0-9]{1,20}'/><br/>  
+      <div class="buttonHolder">
+        <input type='submit' class='staticButtons' id='primaryButton' value = 'Login'/>
+        <input type='submit' formaction='/register' class='staticButtons' id='secondaryButton' value = 'Register'/>
+      </div>
+    </form>
+  </div> 
+  )
 };
 
 export default Login;
