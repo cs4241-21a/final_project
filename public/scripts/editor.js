1efd37f8af607aff3d35bf21314c370e2979fdfa
Matthew <entei678@gmail.com>
2021-10-04 16:27:06 -0400

    Download works.

--- public/scripts/editor.js
+++ public/scripts/editor.js
@@ -1,4 +1,15 @@
 let imgBtn = document.getElementById('imageInput')
+const download = document.getElementById('download')
+const canvas = document.getElementById('imgCanvas')
+let imgData
+
+download.addEventListener('click', function(e) {
+    const link = document.createElement('a')
+    link.download = 'download.png'
+    link.href = canvas.toDataURL()
+    link.click()
+    link.delete()
+});
 imgBtn.addEventListener('change', function(event) {
     if(event.target.files) {//If files
         let imageFile = event.target.files[0]
@@ -8,12 +19,11 @@ imgBtn.addEventListener('change', function(event) {
             const userImage = new Image()
             userImage.src = event.target.result
             userImage.onload = function (event) {
-                const canvas = document.getElementById('imgCanvas')
                 const context = canvas.getContext('2d')
                 canvas.width = userImage.width
                 canvas.height = userImage.height
                 context.drawImage(userImage,0,0)
-                let imgData = canvas.toDataURL("image/jpeg",0.75)
+                imgData = canvas.toDataURL("image/jpeg",0.75)
             }
         }
     }
