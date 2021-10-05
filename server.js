1c1854a980e889e33e16568824ca2d64dd0162f2
ajwhitney23 <ajwhitney@wpi.edu>
2021-10-04 23:47:07 -0400

    fixed db connection issues

--- server.js
+++ server.js
@@ -6,146 +6,148 @@ const cookie = require("cookie-session");
 const helmet = require("helmet");
 const serveStatic = require("serve-static");
 const path = require("path");
+const dontenv = require("dotenv");
 
 const app = express();
 const port = 3000;
 
-// const clientID = "66f056183e981d1a11b2";
+console.log(dontenv.config());
+const clientID = "66f056183e981d1a11b2";
 
 app.use(helmet());
-app.use(serveStatic(path.join(__dirname, 'static')));
+app.use(serveStatic(path.join(__dirname, "static")));
 app.use(express.static("build"));
 app.use(express.json());
 
-// app.use(
-//   cookie({
-//     name: "session",
-//     keys: [process.env.COOKIE_KEY1, process.env.COOKIE_KEY2],
-//   })
-// );
+app.use(
+  cookie({
+    name: "session",
+    keys: [process.env.COOKIE_KEY1, process.env.COOKIE_KEY2],
+  })
+);
 
-// passport.serializeUser(function (user, done) {
-//   done(null, user);
-// });
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
 
-// passport.deserializeUser(function (user, done) {
-//   done(null, user);
-// });
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
-// passport.use(
-//   new GitHubStrategy(
-//     {
-//       clientID: clientID,
-//       clientSecret: process.env.GITHUB_SECRET,
-//       callbackURL: "http://localhost:8080/github/callback",
-//     },
-//     function (accessToken, refreshToken, profile, done) {
-//       return done(null, profile);
-//     }
-//   )
-// );
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: clientID,
+      clientSecret: process.env.GITHUB_SECRET,
+      callbackURL: "http://localhost:8080/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// // authentication handling using passport
-// app.get("/auth/error", (req, res) => res.send("Unknown Error"));
-// app.get(
-//   "/github/callback",
-//   passport.authenticate("github", { failureRedirect: "/auth/error" }),
-//   function (req, res) {
-//     res.redirect("/res?id=" + req.user.id);
-//   }
-// );
+// authentication handling using passport
+app.get("/auth/error", (req, res) => res.send("Unknown Error"));
+app.get(
+  "/github/callback",
+  passport.authenticate("github", { failureRedirect: "/auth/error" }),
+  function (req, res) {
+    res.redirect("/res?id=" + req.user.id);
+  }
+);
 
-// app.get("/res", (req, res) => {
-//   req.session.id = req.query.id;
-//   res.redirect("/");
-// });
+app.get("/res", (req, res) => {
+  req.session.id = req.query.id;
+  res.redirect("/");
+});
 
-// app.get("/id", (req, res) => {
-//   res.send(req.session.id);
-// });
+app.get("/id", (req, res) => {
+  res.send(req.session.id);
+});
 
-// app.get("/logout", (req, res) => {
-//   req.session.id = "";
-//   req.logout();
-//   res.redirect("/");
-// });
+app.get("/logout", (req, res) => {
+  req.session.id = "";
+  req.logout();
+  res.redirect("/");
+});
 
-// const uri =
-//   "mongodb+srv://" +
-//   process.env.DB_USERNAME +
-//   ":" +
-//   process.env.DB_PASSWORD +
-//   "@cluster0.yb8hn.mongodb.net/";
+const uri =
+  "mongodb+srv://" +
+  process.env.DB_USERNAME +
+  ":" +
+  process.env.DB_PASSWORD +
+  "@group8data.ehg8z.mongodb.net/";
 
-// const client = new mongodb.MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// let collection = null;
+const client = new mongodb.MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+let collection = null;
 
-// client
-//   .connect()
-//   .then(() => {
-//     // will only create collection if it doesn't exist
-//     return client.db("gameapi").collection("favorites");
-//   })
-//   .then((__collection) => {
-//     // store reference to collection
-//     collection = __collection;
-//     // blank query returns all documents
-//     return collection.find({}).toArray();
-//   });
+client
+  .connect()
+  .then(() => {
+    // will only create collection if it doesn't exist
+    return client.db("gameapi").collection("favorites");
+  })
+  .then((__collection) => {
+    // store reference to collection
+    collection = __collection;
+    // blank query returns all documents
+    return collection.find({}).toArray();
+  });
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// app.get("/favorites", (req, res) => {
-//   if (collection !== null) {
-//     collection
-//       .find({ userID: req.session.id })
-//       .toArray()
-//       .then((result) => res.json(result));
-//   }
-// });
+app.get("/favorites", (req, res) => {
+  if (collection !== null) {
+    collection
+      .find({ userID: req.session.id })
+      .toArray()
+      .then((result) => res.json(result));
+  }
+});
 
-// app.post("/submit", (req, res) => {
-//   // assumes only one object to insert
-//   const dataJSON = req.body;
-//   dataJSON.userID = req.session.id;
-//   collection
-//     .findOne({
-//       // data to check if favorite already exists we can figure this out later
-//       userID: dataJSON.userID,
-//     })
-//     .then(function (result) {
-//       return result;
-//     })
-//     .then(function (data) {
-//       if (data === null) {
-//         collection.insertOne(dataJSON).then((result) => res.json(result));
-//       } else {
-//         collection
-//           .replaceOne(
-//             {
-//               name: dataJSON.name,
-//               team: dataJSON.team,
-//               userID: dataJSON.userID,
-//             },
-//             dataJSON
-//           )
-//           .then((result) => res.json(result));
-//       }
-//     });
-// });
+app.post("/submit", (req, res) => {
+  // assumes only one object to insert
+  const dataJSON = req.body;
+  dataJSON.userID = req.session.id;
+  collection
+    .findOne({
+      // data to check if favorite already exists we can figure this out later
+      userID: dataJSON.userID,
+    })
+    .then(function (result) {
+      return result;
+    })
+    .then(function (data) {
+      if (data === null) {
+        collection.insertOne(dataJSON).then((result) => res.json(result));
+      } else {
+        collection
+          .replaceOne(
+            {
+              name: dataJSON.name,
+              team: dataJSON.team,
+              userID: dataJSON.userID,
+            },
+            dataJSON
+          )
+          .then((result) => res.json(result));
+      }
+    });
+});
 
-// app.post("/remove", (req, res) => {
-//   // removes an instance matching request body
-//   collection.deleteOne(req.body).then((result) => res.json(result));
-// });
+app.post("/remove", (req, res) => {
+  // removes an instance matching request body
+  collection.deleteOne(req.body).then((result) => res.json(result));
+});
 
 const listener = app.listen(process.env.PORT || port, () => {
   console.log("Your app is listening on port " + listener.address().port);
