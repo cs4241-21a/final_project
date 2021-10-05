77cb3cbd500793eb32b64929a474bd25271a161b
Jack Sullivan <jhaldisul@gmail.com>
2021-10-05 17:54:16 -0400

    got login / signup frontend functionality 99% done

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,27 +1,90 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import useUser from "../hooks/useUser";
 
-import "./css/login_style.css"
+import "./css/login_style.css";
 
 const Login = (props) => {
-  return ( 
-    <div id = 'loginPage'>
-    <div id = 'logoDiv'>
-      <img id = 'logo' src = "./components/resources/logo.jpg" alt = 'App Logo' width="300px" height="300px" title="App Logo"/>
-    </div>
+  const [user, setUser] = useUser();
 
-    <form id = 'form' action='/login' method='POST'>
-      <label for='username'> Username: </label><br/>  
-      <input type='text' id = 'username' name = 'username' required='required' pattern='[A-Za-z0-9]{1,20}'/> <br/>  
+  const submitLogin = async (e) => {
+    console.log("Submitting login");
+    e.preventDefault();
 
-      <label for='password'>Password:</label><br/>  
-      <input type='password' id='password' name='password' required='required' pattern='[A-Za-z0-9]{1,20}'/><br/>  
-      <div class="buttonHolder">
-        <input type='submit' class='staticButtons' id='primaryButton' value = 'Login'/>
-        <input type='submit' formaction='/register' class='staticButtons' id='secondaryButton' value = 'Register'/>
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log({ username, password });
+
+    const res = await fetch("/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (res.status === 400) {
+      alert("Bad login fuck u");
+      return;
+    }
+
+    const userInfo = await res.json();
+    setUser(userInfo);
+  };
+
+  if (user === "loading") return <div>Loading</div>;
+  if (!user) {
+    return (
+      <div id="loginPage">
+        <div id="logoDiv">
+          <img
+            id="logo"
+            src="./components/resources/logo.jpg"
+            alt="App Logo"
+            width="300px"
+            height="300px"
+            title="App Logo"
+          />
+        </div>
+
+        <form id="form" onsubmit="return false">
+          <label for="username"> Username: </label>
+          <br />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required="required"
+            pattern="[A-Za-z0-9]{1,20}"
+          />{" "}
+          <br />
+          <label for="password">Password:</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required="required"
+            pattern="[A-Za-z0-9]{1,20}"
+          />
+          <br />
+          <div class="buttonHolder">
+            <button
+              onClick={submitLogin}
+              class="staticButtons"
+              id="primaryButton"
+            >
+              Login
+            </button>
+            <a href="/signup" class="staticButtons" id="secondaryButton">
+              Sign up
+            </a>
+          </div>
+        </form>
       </div>
-    </form>
-  </div> 
-  )
+    );
+  } else {
+    return <Redirect to="/" />;
+  }
 };
 
 export default Login;
