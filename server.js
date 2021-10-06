2c7e1254d7788c03d0d146c3e75b6a25e5cba951
lewasserman <laurenewasserman@gmail.com>
2021-10-06 10:30:34 -0400

    addList and addItem pages

--- server.js
+++ server.js
@@ -3,8 +3,9 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const express = require("express"),
+  app = express(),
+  port = 3000;
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -15,7 +16,15 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/addList", (request, response) => {
+  response.sendFile(__dirname + "/views/addList.html");
+});
+
+app.get("/addItem", (request, response) => {
+  response.sendFile(__dirname + "/views/addItem.html");
+});
+
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(process.env.PORT || port, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
