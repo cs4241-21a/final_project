3d81a37b614dce150882e0d486d61e26d8f03643
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-10-07 16:41:46 -0400

    Add background silhouette to HomePage

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import PartySilhouetteIcon from "../../components/icons/PartySilhouetteIcon";
 
 export default function HomePage() {
     return (
-        <div>
+        <div className="home-page">
+            <PartySilhouetteIcon />
             <div>Hello world!</div>
         </div>
     );
