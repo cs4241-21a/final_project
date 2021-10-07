12f17fbbd31a5208e5e0e202fc3ecf515de157b4
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-10-07 17:19:26 -0400

    Add GenreCard component without image

---
+++ client/src/components/GenreCard/GenreCard.js
@@ -0,0 +1,9 @@
+import classNames from "classnames";
+
+export default function GenreCard(props) {
+    return (
+        <div className="genre-card">
+            <span className="genre-card">{ props.label.toUpperCase() }</span>
+        </div>
+    );
+}
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PartySilhouetteIcon from "../../components/icons/PartySilhouetteIcon";
+import GenreCard from "../../components/GenreCard/GenreCard";
 
 export default function HomePage() {
     return (
@@ -7,6 +8,12 @@ export default function HomePage() {
             <PartySilhouetteIcon />
             <div className="home-page__title">Create your next party playlist!</div>
             <div className="home-page__subtitle">Choose a genre</div>
+            <div className="home-page__genres">
+                <GenreCard genre="hip-hop" label="Hip Hop" />
+                <GenreCard genre="hard-rock" label="Hard Rock" />
+                <GenreCard genre="electronica" label="Techno" />
+                <GenreCard genre="club" label="Club" />
+            </div>
         </div>
     );
 }
