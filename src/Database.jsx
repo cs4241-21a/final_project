5758002f644394cafe4847a46b0671ba52f20831
gdgarsson <65737765+gdgarsson@users.noreply.github.com>
2021-10-07 16:36:02 -0400

    Update Database.jsx
    Updated Database.jsx to have prototype functions.

--- src/Database.jsx
+++ src/Database.jsx
@@ -1,61 +1,124 @@
-// function addCalendar(calID, calName, calColor) {
-//     let cal = {
-//         parent = getCalendarByID(calID),
-//         children = [],
-//         name = calName,
-//         color = calColor
-//     }
+/*function addCalendar(calID, calName, calColor) {
+    let cal = {
+        parent = getCalendarByID(calID),
+        children = [],
+        name = calName,
+        color = calColor
+    }
 
-//     // Mongo push cal to the calendar collection
-// }
+    fetch('/addCalendar', {
+        method: 'post',
+        body: JSON.stringify(cal)
+    }).then(function(response){
+        return response.text(); // returns either the objectID on success, or an error message on failure
+    }).then(function(response){
+        console.log(response.text());
+    })
+}
 
-// function removeCalendar(calID) {
-//     // Do the same thing as get calendar by ID, but perform a app.remove DeleteOne instead
-// }
+function removeCalendar(calID) {
+    fetch('/removeCalendar', {
+        method: 'post',
+        body: calID
+    }).then(function(response){
+        return response.text(); // Returns either a success or failure message, depending on outcome
+    }).then(function(response){
+        console.log(response.text());
+    })
+}
 
-// function getCalendarByID(calID) {
-//     if (calID === NULL) {
-//         return calID;
-//     }
-//     // perform Mongo/Express app.get to check if calendar exists
+// TODO: Prototype function, do not use yet
+function getCalendarByID(calID) {
+    if (calID === NULL) {
+        return calID;
+    }
 
-//     // If the calendar specified by the id exists, 
-//     // return calID
-//     // else
-//     // return NULL
-// }
+    fetch('/getCalendar', {
+        method: 'post',
+        body: calID
+    }).then(function(response){
+        return response.text(); // Return the calendar ID if exists, return error if else
+    }).then(function(response){
+        console.log(response.text());
+    })
+}
 
-// function createEvent(uID, eDate, eName, eStartTime, eEndTime, eAllDay) {
-//     let event = {
-//         userID = uID,
-//         date = eDate,
-//         name = eName,
-//         //repeat = eRepeat, // depending on if repeat is "WEEKLY", "DAILY", "MONTHLY", or NULL, create additional tasks and push those
-//         startTime = eStartTime, // will be set to 12:00am on the specified date if allDay is true
-//         endTime = eEndTime, // will be set to NULL if allDay is true
-//         allDay = eAllDay
-//     }
+function modifyCalendar(calID, calName, calColor){
+    let cal = {
+        parent: getCalendarByID(calID),
+        children: [],
+        name: calName,
+        color: calColor
+    }
 
-//     // Mongo push to events collection
-// }
+    fetch('/modifyCalendar', {
+        method: 'post',
+        body: JSON.stringify(cal)
+    }).then(function(response){
+        return response.text(); // returns either the objectID of the modified calendar on success, or an error message on failure
+    }).then(function(response){
+        console.log(response.text());
+    })
+}
 
-// function removeEvent(uID, eID) {
-//     // Perform Mongo app.remove DeleteOne for the specified event with the given userID and eventID
-// }
+function createEvent(uID, eDate, eName, eStartTime, eEndTime, eAllDay) {
+    let event = {
+        userID: uID,
+        date: eDate,
+        name: eName,
+        //repeat = eRepeat, // depending on if repeat is "WEEKLY", "DAILY", "MONTHLY", or NULL, create additional tasks and push those
+        startTime: eStartTime, // will be set to 12:00am on the specified date if allDay is true
+        endTime: eEndTime, // will be set to NULL if allDay is true
+        allDay: eAllDay
+    }
 
-// function createTask(uID, tDueDate, tName, tDueTime) {
-//     let task = {
-//         userID = uID,
-//         date = tDueDate,
-//         name = tName,
-//         startTime = NULL,
-//         endTime = tDueTime,
-//         allDay = false
-//     }
+    fetch('/addEvent',{
+        method: 'post',
+        body: JSON.stringify(event)
+    }).then(function(response){
+        return response.text() // return the object ID of the event, error message on failure
+    }).then(function(response){
+        console.log(response.text())
+    })
 
-//     // Mongo push to tasks collection
-// }
+    // Mongo push to events collection
+}
 
-// function removeTask(uID, tID) {
-//     // Perform Mongo app.remove DeleteOne for the specified task with the given userID and taskID
-// }
+function removeEvent(uID, eID) {
+    let info = {
+        userID: uID,
+        eventID: eID
+    }
+
+    fetch('/deleteEvent', {
+        method: 'post',
+        body: JSON.stringify(info)
+    }).then(function(response){
+        return response.text() // return a success message upon deletion, or error message if failure
+    }).then(function(response){
+        console.log(reponse.text())
+    })
+}
+
+function modifyEvent(uID, eDate, eName, eStartTime, eEndTime, eAllDay) {
+    let event = {
+        userID: uID,
+        date: eDate,
+        name: eName,
+        //repeat = eRepeat, // depending on if repeat is "WEEKLY", "DAILY", "MONTHLY", or NULL, create additional tasks and push those
+        startTime: eStartTime, // will be set to 12:00am on the specified date if allDay is true
+        endTime: eEndTime, // will be set to NULL if allDay is true
+        allDay: eAllDay
+    }
+
+    fetch('/modifyEvent',{
+        method: 'post',
+        body: JSON.stringify(event)
+    }).then(function(response){
+        return response.text() // return the object ID of the modified event, error message on failure
+    }).then(function(response){
+        console.log(response.text())
+    })
+
+    // Mongo push to events collection
+}*/
