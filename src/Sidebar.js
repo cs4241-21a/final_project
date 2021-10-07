6300f20bb9f2f4b4b2f27837b5989e202fca78ba
justgaret23 <sheehan.garrett23@gmail.com>
2021-10-07 13:02:28 -0500

    add form

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 class Sidebar extends Component {
   constructor(props) {
@@ -12,6 +14,38 @@ class Sidebar extends Component {
       <div className='sidebar'>
         <div className='sidebar-calendars'>
           <p>Calendars</p>
+          <Popup trigger={<button>Add Event</button>}>
+            {close => (
+              <div classname="eventSubmit">
+                <form action= '/createEvent' classname="eventForm" method="POST">
+                  <label>Create your event here!</label>
+                  <label>Event Name</label>
+                  <div>
+                    <input type='text' name='event-name' placeholder="Event Name"></input>
+                  </div>
+
+                  <label>Event Date</label>
+                  <div>
+                    <input type='date' name='event-date' placeholder="Event Date"></input>
+                  </div>
+
+                  <label>Start Time</label>
+                  <div>
+                    <input type='time' name='event-start-time'></input>
+                  </div>
+
+                  <label>End Time</label>
+                  <div>
+                    <input type='time' name='event-end-time'></input>
+                  </div>
+                  
+                  <div class="eventButton">
+                    <button id="event-create">Create Event</button>
+                  </div>
+                </form>
+              </div>
+            )}
+          </Popup>
         </div>
       </div>
       );
