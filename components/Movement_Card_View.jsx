2a3e669f76d376b9784d1e028950b7d2047d8e88
Jack Sullivan <jhaldisul@gmail.com>
2021-10-08 13:34:30 -0400

    idek

--- components/Movement_Card_View.jsx
+++ components/Movement_Card_View.jsx
@@ -2,57 +2,70 @@ import React from "react";
 import Card_Row from "./Card_Row";
 
 const Movement_Card_View = (props) => {
-    console.log(props.movement)
-    console.log(props.parent)
+  console.log(props.movement);
+  console.log(props.parent);
   // return <div>movement card</div>;
-  console.log("React Log page")
+  console.log("React Log page");
 
   const editEntry = () => {
-      document.getElementById('addModifyButton').value = 'Edit'
-      document.getElementById('movementName').value = props.movement.movementName
-      document.getElementById('numberOfSets').value = props.movement.sets.length
+    document.getElementById("addModifyButton").innerText = "Edit";
+    document.getElementById("movementName").value = props.movement.movementName;
+    document.getElementById("numberOfSets").value = props.movement.sets.length;
 
-      props.parent.state.newNumSets = props.movement.sets.length 
-      let setsDiv = document.getElementById('setsDiv');
+    props.parent.state.newNumSets = props.movement.sets.length;
+    let setsDiv = document.getElementById("setsDiv");
 
-    movementName = props.movement.movementName
-    
-    let children = ''
+    const movementName = props.movement.movementName;
 
-      for (let i = 0; i < props.movement.sets.length; ++i) {
-        let newWeight = document.getElementById(`${movementName}weight${i}`).innerHTML
-        let newReps = document.getElementById(`${movementName}reps${i}`).innerHTML
-        let newRPE = document.getElementById(`${movementName}RPE${i}`).innerHTML
+    let children = "";
 
-        children += `<div id='set${i}'> 
+    for (let i = 0; i < props.movement.sets.length; ++i) {
+      let newWeight = document.getElementById(
+        `${movementName}weight${i}`
+      ).innerHTML;
+      let newReps = document.getElementById(
+        `${movementName}reps${i}`
+      ).innerHTML;
+      let newRPE = document.getElementById(`${movementName}RPE${i}`).innerHTML;
+
+      children += `<div id='set${i}'> 
                         <input id='weight${i}' placeholder="weight" value="${newWeight}"></input> 
                         <input id='reps${i}' placeholder="reps" value="${newReps}"></input> 
-                        <input id='RPE${i}' placeholder="RPE" value="${newReps}"></input> 
-                    </div>`
-      }
-      setsDiv.innerHTML = children
+                        <input id='RPE${i}' placeholder="RPE" value="${newRPE}"></input> 
+                    </div>`;
+    }
+    setsDiv.innerHTML = children;
 
-      console.log('Children is: ' + children)
-  }
+    console.log("Children is: " + children);
+  };
 
-  let cardRows = []
+  let cardRows = [];
   for (let i = 0; i < props.movement.sets.length; ++i) {
-    cardRows.push(<Card_Row exercise_name={props.movement.movementName} setNumber={i} set={props.movement.sets[i]}></Card_Row>)
+    cardRows.push(
+      <Card_Row
+        exercise_name={props.movement.movementName}
+        setNumber={i}
+        set={props.movement.sets[i]}
+      ></Card_Row>
+    );
   }
-    return <div>
-        <p id={props.movement.movementName}>{props.movement.movementName}</p>
-        <p id={props.movement.movementName +'numSets'}>{props.movement.sets.length}</p>
-        <table>
-            <tr>
-                <th>Weight</th>
-                <th>Reps</th>
-                <th>RPE</th>
-            </tr>
-            {cardRows}
-        </table>
-    <button onClick = {editEntry}>Edit This Entry</button>
+  return (
+    <div>
+      <p id={props.movement.movementName}>{props.movement.movementName}</p>
+      <p id={props.movement.movementName + "numSets"}>
+        {props.movement.sets.length}
+      </p>
+      <table>
+        <tr>
+          <th>Weight</th>
+          <th>Reps</th>
+          <th>RPE</th>
+        </tr>
+        {cardRows}
+      </table>
+      <button onClick={editEntry}>Edit This Entry</button>
     </div>
+  );
 };
-
 
 export default Movement_Card_View;
