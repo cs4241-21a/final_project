266fd52a062b58bc6ae9115c9bff767760a819dc
Errica <errica.cheng@gmail.com>
2021-10-08 16:56:47 -0400

    wowah menu button

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MainRoute from "./routes/MainRoute";
+import MenuContainer from "./components/MenuContainer";
+
+ReactDOM.render(
+    <MenuContainer />,
+    document.querySelector("#container")
+);
 
 const mountNode = document.getElementById("app");
 ReactDOM.render(<MainRoute name="test" />, mountNode);
