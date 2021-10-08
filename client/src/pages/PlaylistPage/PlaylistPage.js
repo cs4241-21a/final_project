c177e66a0008ceae5d0365c13b07f089939360fe
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-10-07 21:06:50 -0400

    Add details bar to PlaylistPage

--- client/src/pages/PlaylistPage/PlaylistPage.js
+++ client/src/pages/PlaylistPage/PlaylistPage.js
@@ -1,5 +1,15 @@
 export default function PlaylistPage() {
     return (
-      <div>Hello world!</div>
+      <div className="playlist-page">
+          <div className="playlist-page__details">
+              <div className="details__left">
+                  <div className="details__title">Hip Hop Party Playlist</div>
+                  <div className="details__subtitle">10 songs • 2 hours, 14 minutes</div>
+              </div>
+              <button>
+                  Add to Spotify
+              </button>
+          </div>
+      </div>
     );
 }
