25731de2f724e89a45eebf2704abe9057823c96e
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-10-07 20:19:46 -0400

    Add background images to GenreCard components, fix dev script

--- client/src/components/GenreCard/GenreCard.js
+++ client/src/components/GenreCard/GenreCard.js
@@ -1,7 +1,8 @@
 export default function GenreCard(props) {
     return (
-        <div className="genre-card">
-            <span className="genre-card">{ props.label.toUpperCase() }</span>
+        <div className="genre-card" style={{ backgroundImage: `url(/images/genres/${props.genre}.png` }}>
+            <div className="genre-card__color" style={{ backgroundColor: props.color }} />
+            <div className="genre-card__text">{ props.label.toUpperCase() }</div>
         </div>
     );
 }
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -9,10 +9,10 @@ export default function HomePage() {
             <div className="home-page__title">Create your next party playlist!</div>
             <div className="home-page__subtitle">Choose a genre</div>
             <div className="home-page__genres">
-                <GenreCard genre="hip-hop" label="Hip Hop" />
-                <GenreCard genre="hard-rock" label="Hard Rock" />
-                <GenreCard genre="electronica" label="Techno" />
-                <GenreCard genre="club" label="Club" />
+                <GenreCard genre="hip-hop" label="Hip Hop" color="#CE1509" />
+                <GenreCard genre="hard-rock" label="Hard Rock" color="#09CE9F" />
+                <GenreCard genre="electronic" label="Techno" color="#096FCE" />
+                <GenreCard genre="club" label="Club" color="#CA09CE" />
             </div>
         </div>
     );
