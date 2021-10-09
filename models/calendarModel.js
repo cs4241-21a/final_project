8004099f99b335e89d63da94077dfeb8f1bf1310
Joshua McKeen <jdmckeen@wpi.edu>
2021-10-09 12:43:05 -0400

    minor change of schema terminology

--- models/calendarModel.js
+++ models/calendarModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const calendarSchema = new Schema({
-    owner:{
+    username:{
         type:String,
         required: true
     },
