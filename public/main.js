a77df7f59c3cc4ad847938c32d88a33dcd52fa87
YangLyuJimmy <jimmylyuyang@outlook.com>
2021-10-08 23:49:13 -0400

    final starter code

--- public/main.js
+++ public/main.js
@@ -39,13 +39,33 @@ const app1 = Vue.createApp({
 
 // this app2 is a new Vue used to control <div id="app2"> in html
 const app2 = Vue.createApp({
-    data: () => ({
-        counter: 0
-    }),
+    data() {
+        return {
+            counter: 0
+        }
+    },
     methods: {
         add() {
             this.counter++;
         },
-        
+        sendRequest() {
+            // window.location.replace('/aboutus');
+            var json = { name: "wangyonghua" },
+            body = JSON.stringify( json );
+            fetch( '/addData', {
+                method:'POST',
+                body,
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              })
+              .then( response => response.json() )
+              .then( function( response ) {
+                console.log(response.result);
+              })
+        }
+    },
+    created() {
+        this.sendRequest()
     }
 })
