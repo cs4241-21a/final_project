220c9b7c0c77994323c8e1d85f4bdb83a21c988a
Errica <errica.cheng@gmail.com>
2021-10-09 18:11:01 -0400

    calendar display pretty :)

--- src/components/FarmingDisplay.tsx
+++ src/components/FarmingDisplay.tsx
@@ -20,7 +20,7 @@ const FarmingDisplay = ({
       (e) => e.day_of_week.find((d) => d === "mon") !== undefined
     )
   );
-  const [tueLocs, setTueLoc] = React.useState<FarmingSpotProps[]>(
+/*  const [tueLocs, setTueLoc] = React.useState<FarmingSpotProps[]>(
     locations.filter(
       (e) => e.day_of_week.find((d) => d === "tue") !== undefined
     )
@@ -29,16 +29,104 @@ const FarmingDisplay = ({
     locations.filter(
       (e) => e.day_of_week.find((d) => d === "wed") !== undefined
     )
-  );
+  );*/
   const [monFarm, setMonFarm] = React.useState<FarmableProps[]>(
     farmables.filter((e) => monLocs.some((l) => l.name === e.farm_at))
   );
-  const [tueFarm, setTueFarm] = React.useState<FarmableProps[]>(
+/*  const [tueFarm, setTueFarm] = React.useState<FarmableProps[]>(
     farmables.filter((e) => tueLocs.some((l) => l.name === e.farm_at))
   );
   const [wedFarm, setWedFarm] = React.useState<FarmableProps[]>(
     farmables.filter((e) => wedLocs.some((l) => l.name === e.farm_at))
-  );
+  );*/
+
+    const [wedLocs, setWedLoc] = React.useState<FarmingSpotProps[]>(
+        [
+            {
+                fullname: { en: "Domain" },
+                day_of_week: ["wed, sat"],
+                resin: 5,
+                name: "test",
+                type: "idk"
+            },
+            {
+                fullname: { en: "Domain 2" },
+                day_of_week: ["wed, sat"],
+                resin: 3,
+                name: "test2",
+                type: "idk"
+            }
+        ]
+    );
+
+    const [tueLocs, setTueLoc] = React.useState<FarmingSpotProps[]>(
+        [
+            {
+                fullname: { en: "Domain" },
+                day_of_week: ["tue, fri"],
+                resin: 5,
+                name: "test",
+                type: "idk"
+            },
+            {
+                fullname: { en: "Domain 2" },
+                day_of_week: ["tue, fri"],
+                resin: 3,
+                name: "test2",
+                type: "idk"
+            }
+        ]
+    );
+
+    const [wedFarm, setWedFarm] = React.useState<FarmableProps[]>(
+        [
+            {
+                name: "farmable1",
+                fullname: { en: "Emblem of Severed Fate" },
+                farm_at: "test",
+            },
+            {
+                name: "farmable3",
+                fullname: { en: "Farmable Three" },
+                farm_at: "test",
+            },
+            {
+                name: "farmable4",
+                fullname: { en: "Farmable Four" },
+                farm_at: "test2",
+            },
+        ]
+    );
+
+    const [tueFarm, setTueFarm] = React.useState<FarmableProps[]>(
+        [
+            {
+                name: "farmable1",
+                fullname: { en: "Emblem of Severed Fate" },
+                farm_at: "test",
+            },
+            {
+                name: "farmable2",
+                fullname: { en: "Farmable Two" },
+                farm_at: "test",
+            },
+            {
+                name: "farmable3",
+                fullname: { en: "Farmable Three" },
+                farm_at: "test",
+            },
+            {
+                name: "farmable4",
+                fullname: { en: "Farmable Four" },
+                farm_at: "test2",
+            },
+            {
+                name: "farmable5",
+                fullname: { en: "Farmable Five" },
+                farm_at: "test2",
+            },
+        ]
+    );
 
   return (
     // !!! TODO (UI): Create and implement JSX components for FarmingDisplay
--- src/components/primitives/DayDisplay.tsx
+++ src/components/primitives/DayDisplay.tsx
@@ -14,6 +14,7 @@ const DayDisplay = (props: {
         <div className="day-title">{props.days}</div>
         {props.locations.map(e => {
             let mats = props.farmables.filter(m => m.farm_at === e.name);
+            /*let imgSrc = "/img/farmables/" + mats.name + ".png";*/
             return (
                 <div className="loc">
                     <h2>{e.fullname.en}</h2>
@@ -21,7 +22,7 @@ const DayDisplay = (props: {
                         return (
                             <div>
                                 {/* TODO: Have this get the image of that material */}
-                                <img className="icon" src="/img/artifacts/Adventurer.png" /> 
+                                <img className="icon" src="/img/farmables/Adventurer.png" alt={mat.fullname.en}/>
                                 <p>{mat.fullname.en}</p>
                             </div>
                         )
