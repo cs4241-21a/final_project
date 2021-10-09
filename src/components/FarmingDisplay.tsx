8c42ffe6e75f21811f501c3df64aacb6ec68f143
Errica <errica.cheng@gmail.com>
2021-10-09 18:12:24 -0400

    commented out dummy data

--- src/components/FarmingDisplay.tsx
+++ src/components/FarmingDisplay.tsx
@@ -20,7 +20,7 @@ const FarmingDisplay = ({
       (e) => e.day_of_week.find((d) => d === "mon") !== undefined
     )
   );
-/*  const [tueLocs, setTueLoc] = React.useState<FarmingSpotProps[]>(
+  const [tueLocs, setTueLoc] = React.useState<FarmingSpotProps[]>(
     locations.filter(
       (e) => e.day_of_week.find((d) => d === "tue") !== undefined
     )
@@ -29,18 +29,19 @@ const FarmingDisplay = ({
     locations.filter(
       (e) => e.day_of_week.find((d) => d === "wed") !== undefined
     )
-  );*/
+  );
   const [monFarm, setMonFarm] = React.useState<FarmableProps[]>(
     farmables.filter((e) => monLocs.some((l) => l.name === e.farm_at))
   );
-/*  const [tueFarm, setTueFarm] = React.useState<FarmableProps[]>(
+  const [tueFarm, setTueFarm] = React.useState<FarmableProps[]>(
     farmables.filter((e) => tueLocs.some((l) => l.name === e.farm_at))
   );
   const [wedFarm, setWedFarm] = React.useState<FarmableProps[]>(
     farmables.filter((e) => wedLocs.some((l) => l.name === e.farm_at))
-  );*/
+  );
 
-    const [wedLocs, setWedLoc] = React.useState<FarmingSpotProps[]>(
+    /**THIS CHUNK IS DUMMY DATA FOR TESTING PURPOSES -- REMOVE WHEN API IS CONNECTED**/
+/*    const [wedLocs, setWedLoc] = React.useState<FarmingSpotProps[]>(
         [
             {
                 fullname: { en: "Domain" },
@@ -126,7 +127,7 @@ const FarmingDisplay = ({
                 farm_at: "test2",
             },
         ]
-    );
+    );*/
 
   return (
     // !!! TODO (UI): Create and implement JSX components for FarmingDisplay
