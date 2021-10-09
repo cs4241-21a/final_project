4a56e3aed30d38f9332377da9e597ca742903b86
CAVP <cavp@autoreg-170481.dyn.wpi.edu>
2021-10-09 11:37:36 -0400

    register functionality

---
+++ controllers/auth.js
@@ -0,0 +1,49 @@
+const   mysql = require("mysql");
+const     jwt = require('jsonwebtoken');
+const  bcrypt = require('bcryptjs');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+  });
+
+exports.register = (request, response) => {
+    console.log(request.body);
+
+    const {name, email, password, passwordConfirm} = request.body;
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+        if(error){
+            console.log(error);
+        }
+
+        if(results.length > 0) {
+            return response.render ('register', {
+                //email used already
+            })
+        } else if ( password !== passwordConfirm) {
+            return response.render ('register',{
+                //passwords dont match
+            })
+        }
+
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+        db.query('INSERT INTO users SET ?', {name:name, email:email, password:hashedPassword}, (error, results) => {
+            if(error){
+                console.log(error);
+            } else {
+                return response.render('register', {
+                    //user registered
+                })
+            }
+        })
+
+
+    });
+
+}
--- server.js
+++ server.js
@@ -1,20 +1,63 @@
-// server.js
-// where your node app starts
+//
+const express = require("express");
+const     app = express();
+const    port = 3000;
+const   mysql = require("mysql");
+const  dotenv = require('dotenv');
+const  path = require("path");
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express"),
-  app = express(),
-  port = 3000;
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory));
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+dotenv.config({ path: './.env'});
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
 });
+
+ db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!"); 
+/*   var sqlUserTable = "CREATE TABLE Users ( username VARCHAR2(20) Primary Key, password VARCHAR2(30))";
+  var sqlListsTable = "CREATE TABLE Lists (listName VARCHAR2(30) Primary Key, description VARCHAR2(100), username VARCHAR2(20) References Users (username))";
+  var sqlItemsTable = "CREATE TABLE Items (itemName VARCHAR2(50) Primary Key, link VARCHAR2(300) Primary Key, price NUMBER(6,2), store VARCHAR2(30), picture VARCHAR2(100), listName VARCHAR2(30) References Lists (listName))";
+
+  db.query(ssqlUserTableql, function (err, result) {
+    if (err) throw err;
+    console.log("User Table created");
+  });
+
+  db.query(ssqlListsTableql, function (err, result) {
+    if (err) throw err;
+    console.log("Lists Table created");
+  });
+
+  db.query(ssqlItemsTableql, function (err, result) {
+    if (err) throw err;
+    console.log("Items Table created");
+  });  */
+});
+
+
+// ROUTES
+
+app.get("/", (request, response) => {
+  response.render("index");
+});
+
+app.get("/register", (request, response) => {
+  response.render("register");
+});
+
+app.use('/auth', require('./routes/auth'));
 
 app.get("/addList", (request, response) => {
   response.sendFile(__dirname + "/views/addList.html");
