a60eea0b9c404b9543f4f9304edb804ef2ae983c
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-10-10 18:47:26 -0400

    Add more comments to server.js

--- server.js
+++ server.js
@@ -7,39 +7,46 @@ import SpotifyService from "./SpotifyService.js";
 const port = 5000;
 const dir = `${path.resolve()}/client/build`;
 
-// server
+// SERVER
 const server = express();
 server.listen(port);
-
-// middleware
 server.use(express.static(dir)); // server static client build files
 
-// dev routes
+// DEV ROUTES
+
+// redirect to Spotify auth portal for retrieving a refresh token
 server.get('/dev/token', async (req, res) => {
     res.redirect(SpotifyService.getRefreshTokenURL());
 });
 
+// capture refresh token via Spotify auth portal callback
 server.get('/callback', async (req, res) => {
     const token = await SpotifyService.getRefreshToken(req.query.code);
     res.json(`Here is your new refresh token: ${token}. Place this in process.env.REFRESH_TOKEN for private Spotify API authorization.`);
 });
 
-// api routes
+// API ROUTES
+
+// get access token for authenticating Spotify Web Player
 server.get('/api/token', async (req, res) => {
    res.json(await SpotifyService.getAccessToken());
 });
 
+// play song on Spotify Web Player
 server.put('/api/player/:id/play', async (req, res) => {
     await SpotifyService.playSong(req.params.id, req.query.uri);
     res.send();
 });
 
+// delete song from playlist
 server.delete('/api/playlist/:id', async (req, res) => {
     await SpotifyService.deleteSongsFromPlaylist(req.params.id, [req.query.uri]);
     res.json(await SpotifyService.getSongsFromPlaylist(req.params.id));
 });
 
-// client routes
+// CLIENT ROUTES
+
+// React app redirect
 server.get('*', (req, res) => {
     res.sendFile(`${dir}/index.html`);
 });
