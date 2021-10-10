0bffd2182c2432ce305177bd0f332ecae74dba4d
Benstaw <ben@2broadview.com>
2021-10-10 14:59:31 -0400

    removed ics button for unfinished events

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -27,7 +27,7 @@ const loadEvents = function(){
             let td2 = document.createElement("td");
             let td3 = document.createElement("td");
             let td4 = document.createElement("td");
-            let td5 = document.createElement("td");
+            let td5 = null
             let item = document.createTextNode(json[count].eventName);
             let item2 = document.createTextNode("No date chosen");
             let item3 = document.createTextNode("No time chosen");
@@ -36,28 +36,35 @@ const loadEvents = function(){
                 item2 = document.createTextNode(json[count].chosenEventDate);
                 item3 = document.createTextNode(json[count].chosenStartTime);
             }
-            let item5 = document.createElement("a")
+            let item5 = null
+            let allowButton = true
+            if(json[count].chosenEventDate === null){allowButton = false}
+            
+           if(allowButton === true){
+            td5 = document.createElement("td");
+            item5 = document.createElement("a")
             let button = document.createElement("button")
             button.innerHTML = "Click for ics"
-            
             item5.appendChild(button)
 
             let file = createFile(json[count])
             item5.setAttribute("href", file)
             
             item5.setAttribute("download", "event.ics")
+           }
+            
 
             td1.appendChild(item);
             td2.appendChild(item2);
             td3.appendChild(item3);
             td4.appendChild(item4);
-            td5.appendChild(item5);
+            if(allowButton === true){td5.appendChild(item5)}
 
             tr.appendChild(td1);
             tr.appendChild(td2);
             tr.appendChild(td3);
             tr.appendChild(td4);
-            tr.appendChild(td5);
+            if(allowButton === true){tr.appendChild(td5)}
 
             table.append(tr);
         }
