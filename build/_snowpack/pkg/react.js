409c94711989e2b6297daf4ea03994cbd65e0f44
justgaret23 <64374848+justgaret23@users.noreply.github.com>
2021-10-09 22:42:49 -0400

    die build you dont belong in this world

--- build/_snowpack/pkg/react.js
+++
@@ -1,7 +0,0 @@
-import { r as react } from './common/index-4b0af3e2.js';
-export { r as default } from './common/index-4b0af3e2.js';
-
-
-
-var Component = react.Component;
-export { Component };
--- build/js/globals.js
+++
@@ -1,7 +0,0 @@
-export const GLOBAL_VARIABLES = {
-    events: [],
-    calendars: [],
-    tasks: [],
-    userId: '',
-    selectedCalendarId: ''
-}
