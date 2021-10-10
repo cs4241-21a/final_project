abdd2f2d73d53b2eded6cc9489b2aab05c0a5b20
AstroSeer <matthewnagy@verizon.net>
2021-10-09 22:32:34 -0400

    Update Menu.jsx

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,10 +4,10 @@ import MenuButton from "./MenuButton";
 class Menu extends Component {
     render() {
         var visibility = "hide";
-        document.body.style.backgroundColor = "rgba(73, 133, 158, 1)";
+        //document.body.style.backgroundColor = "rgba(73, 133, 158, 1)";
         if (this.props.menuVisibility) {
             visibility = "show";
-            document.body.style.backgroundColor = "rgba(10, 43, 68, 0.8)";
+            //document.body.style.backgroundColor = "rgba(10, 43, 68, 0.8)";
         }
 
         return (
