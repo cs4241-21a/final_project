6523c2d8b7d600e06322161bc7bb08d0ed096f36
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-10-10 18:34:19 -0400

    Improve Pause icon

--- client/src/components/icons/PauseIcon.js
+++ client/src/components/icons/PauseIcon.js
@@ -1,7 +1,7 @@
 export default function PauseIcon() {
     return (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
+        <path d="M4,21.39H9.32V2.61H4ZM14.68,2.61V21.39h5.37V2.61Z"/>
       </svg>
     );
 }
