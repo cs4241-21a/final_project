f32fa8ea6ec60cdebd7850fa95f73b9a6eeb35f5
Joshua McKeen <jdmckeen@wpi.edu>
2021-10-11 10:13:48 -0400

    changing personal calendar add buttont text

--- build/EventsApp.js
+++ build/EventsApp.js
@@ -8,7 +8,7 @@ class EventsApp extends React.Component {
   render() {
     return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("h2", null, "My Events"), /* @__PURE__ */ React.createElement("hr", null), /* @__PURE__ */ React.createElement(OwnedEventList, null)), /* @__PURE__ */ React.createElement("div", {
       class: "w-100"
-    }), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("h2", null, "Pending Event Invites:"), /* @__PURE__ */ React.createElement("hr", null), /* @__PURE__ */ React.createElement(PendingEventsList, null)));
+    }), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("h2", null, "Event Invites:"), /* @__PURE__ */ React.createElement("hr", null), /* @__PURE__ */ React.createElement(PendingEventsList, null)));
   }
 }
 export default EventsApp;
