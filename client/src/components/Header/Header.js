3a8dea3157291bbf4e9e75b36d6e9052b28d8a43
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-10-10 20:41:47 -0400

    Remove user from header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,10 +5,6 @@ export default function Header() {
       <div className="header">
           <div className="header__framework">
               <Link to="/" className="header__logo">Bopify</Link>
-              <div className="header__user">
-                  <div />
-                  <span>paradoxpyt</span>
-              </div>
           </div>
       </div>
     );
