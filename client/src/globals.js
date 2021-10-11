404bf665a30a3b2a2d370e98d2abef694b6810b5
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-10-10 20:54:24 -0400

    Add Genres global variable

---
+++ client/src/globals.js
@@ -0,0 +1,22 @@
+export const Genres = {
+  'hip-hop': {
+    id: 'hip-hop',
+    label: 'Hip Hop',
+    color: '#CE1509'
+  },
+  'hard-rock': {
+    id: 'hard-rock',
+    label: 'Hard Rock',
+    color: '#09CE9F'
+  },
+  'electronic': {
+    id: 'electronic',
+    label: 'Techno',
+    color: '#096FCE'
+  },
+  'club': {
+    id: 'club',
+    label: 'Club',
+    color: '#CA09CE'
+  }
+}
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,19 +1,17 @@
 import React from "react";
 import PartySilhouetteIcon from "../../components/icons/PartySilhouetteIcon";
 import GenreCard from "../../components/GenreCard/GenreCard";
+import { Genres } from "../../globals";
 
 export default function HomePage() {
-    return (
-        <div className="home-page">
-            <PartySilhouetteIcon />
-            <div className="home-page__title">Create your next party playlist!</div>
-            <div className="home-page__subtitle">Choose a genre</div>
-            <div className="home-page__genres">
-                <GenreCard genre="hip-hop" label="Hip Hop" color="#CE1509" />
-                <GenreCard genre="hard-rock" label="Hard Rock" color="#09CE9F" />
-                <GenreCard genre="electronic" label="Techno" color="#096FCE" />
-                <GenreCard genre="club" label="Club" color="#CA09CE" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="home-page">
+      <PartySilhouetteIcon />
+      <div className="home-page__title">Create your next party playlist!</div>
+      <div className="home-page__subtitle">Choose a genre</div>
+      <div className="home-page__genres">
+        { Object.values(Genres).map(genre => <GenreCard key={ genre.id } genre={ genre.id } label={ genre.label } color={ genre.color } />) }
+      </div>
+    </div>
+  );
 }
